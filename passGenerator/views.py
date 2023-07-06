4bc8ee9855fa179568602724dfcada9acc0315de
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-06 10:45:40 +0200

    created form and prepared for password display

--- passGenerator/views.py
+++ passGenerator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def password(request):
+    return render(request, 'password.html')
